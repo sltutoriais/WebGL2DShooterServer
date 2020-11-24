2c1afd5205e70b8e14f9a74dd238f5b9d2b77715
Sebastião Lucio <sebastiao_coldplay@hotmail.com>
2020-11-24 12:33:15 -0300

    gameInstance line code removed

--- public/client.js
+++ public/client.js
@@ -7,11 +7,7 @@ window.addEventListener('load', function() {
 		if (!socket.isReady) return;
 		
 		var args = Array.prototype.slice.call(arguments, 1);
-		if(gameInstance!=null)
-		{
-		 gameInstance.SendMessage("NetworkManager", method, args.join(':'));
-		}
-		else if(unityInstance!=null)
+		if(unityInstance!=null)
 		{
 		  unityInstance.SendMessage("NetworkManager", method, args.join(':'));
 		}
@@ -20,11 +16,7 @@ window.addEventListener('load', function() {
 
 	socket.on('PONG', function() {
 				      		
-	   if(gameInstance!=null)
-		{
-		  gameInstance.SendMessage ('NetworkManager', 'OnPrintPongMsg');
-		}
-		else if(unityInstance!=null)
+	    if(unityInstance!=null)
 		{
 		  unityInstance.SendMessage ('NetworkManager', 'OnPrintPongMsg');
 		}
@@ -33,11 +25,7 @@ window.addEventListener('load', function() {
 	socket.on('JOIN_SUCCESS', function(id,name,avatar) {
 				      		
 	  var currentUserAtr = id+':'+name+':'+avatar;
-	  if(gameInstance!=null)
-		{
-		 gameInstance.SendMessage ('NetworkManager', 'OnJoinGame', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+	  if(unityInstance!=null)
 		{
 		 unityInstance.SendMessage ('NetworkManager', 'OnJoinGame', currentUserAtr);
 		}
@@ -46,11 +34,7 @@ window.addEventListener('load', function() {
 	socket.on('SPAWN_PLAYER', function(id,name,avatar,dx) {
 				      		
 	  var currentUserAtr = id+':'+name+':'+avatar+':'+dx;
-	 if(gameInstance!=null)
-		{
-		  gameInstance.SendMessage ('NetworkManager', 'OnSpawnPlayer', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+      if(unityInstance!=null)
 		{
 		 unityInstance.SendMessage ('NetworkManager', 'OnSpawnPlayer', currentUserAtr);
 		}
@@ -60,11 +44,7 @@ window.addEventListener('load', function() {
 	socket.on('UPDATE_POS_AND_ROT', function(id,dx,rotation) {
 				      		
 	  var currentUserAtr = id+':'+dx+':'+rotation;
-	  if(gameInstance!=null)
-		{
-		  gameInstance.SendMessage ('NetworkManager', 'OnUpdatePosAndRot', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+	   if(unityInstance!=null)
 		{
 		unityInstance.SendMessage ('NetworkManager', 'OnUpdatePosAndRot', currentUserAtr);
 		}
@@ -74,11 +54,7 @@ window.addEventListener('load', function() {
 	socket.on('UPDATE_JUMP', function(id) {
 				      		
 	  var currentUserAtr = id;
-	  if(gameInstance!=null)
-		{
-		 gameInstance.SendMessage ('NetworkManager', 'OnUpdateJump', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+	   if(unityInstance!=null)
 		{
 		  unityInstance.SendMessage ('NetworkManager', 'OnUpdateJump', currentUserAtr);
 		}
@@ -88,11 +64,7 @@ window.addEventListener('load', function() {
    socket.on('UPDATE_PLAYER_DAMAGE', function(id,health) {
 				      		
 	  var currentUserAtr = id+':'+health;
-	 if(gameInstance!=null)
-		{
-		  gameInstance.SendMessage ('NetworkManager', 'OnUpdatePlayerDamage', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+	  if(unityInstance!=null)
 		{
 		  unityInstance.SendMessage ('NetworkManager', 'OnUpdatePlayerDamage', currentUserAtr);
 		}
@@ -102,11 +74,7 @@ window.addEventListener('load', function() {
  socket.on('GAME_OVER', function(id) {
 				      		
 	  var currentUserAtr = id;
-	 if(gameInstance!=null)
-		{
-		  gameInstance.SendMessage ('NetworkManager', 'OnGameOver', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+	 if(unityInstance!=null)
 		{
 		  unityInstance.SendMessage ('NetworkManager', 'OnGameOver', currentUserAtr);
 		}
@@ -116,11 +84,7 @@ window.addEventListener('load', function() {
  socket.on('UPDATE_ANIMATOR', function(id,animation) {
 				      		
 	  var currentUserAtr = id+':'+animation;
-	  if(gameInstance!=null)
-		{
-		  gameInstance.SendMessage ('NetworkManager', 'OnUpdateAnim', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+	if(unityInstance!=null)
 		{
 		  unityInstance.SendMessage ('NetworkManager', 'OnUpdateAnim', currentUserAtr);
 		}
@@ -130,11 +94,7 @@ window.addEventListener('load', function() {
  socket.on('USER_DISCONNECTED', function(id) {
 				      		
 	  var currentUserAtr = id;
-	  if(gameInstance!=null)
-		{
-		  gameInstance.SendMessage ('NetworkManager', 'OnUserDisconnected', currentUserAtr);
-		}
-		else if(unityInstance!=null)
+	  if(unityInstance!=null)
 		{
 		 unityInstance.SendMessage ('NetworkManager', 'OnUserDisconnected', currentUserAtr);
 		}
